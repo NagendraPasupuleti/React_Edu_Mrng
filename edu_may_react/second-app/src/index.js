f2a254467800c0ea07df910c6097bb59ffc8b613
Nagendra Pasupuleti <nagpasupuleti143@gmail.com>
2022-06-25 10:46:32 +0530

    Error removing from react-dom

--- edu_may_react/second-app/src/index.js
+++ edu_may_react/second-app/src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Routing from "./components/Routing"
+import ReactDOM from 'react-dom/client';
+import Routing from "./components/Routing";
+import { createRoot } from 'react-dom/client';
 
 
 
-ReactDOM.render(<Routing />, document.getElementById('root'));
+//ReactDOM.render(<Routing />, document.getElementById('root'));
+// const container = document.getElementById('root');
+// const root = createRoot(container); // createRoot(container!) if you use TypeScript
+// root.render(<Routing/>);
+const container = document.getElementById('root');
+const root =ReactDOM.createRoot(container);
+root.render(<Routing/>);
